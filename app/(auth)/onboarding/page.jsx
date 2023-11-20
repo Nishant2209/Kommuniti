b241d82822c688fe7e8484c105e653b9a9e5f398
Nishant Kumar Singh <nishant220902@gmail.com>
2023-11-20 15:43:42 +0530

    made the onboarding page

--- app/(auth)/onboarding/page.jsx
+++ app/(auth)/onboarding/page.jsx
@@ -1,7 +1,31 @@
-const page = () => {
+import AccountProfile from "@/components/forms/AccountProfile";
+import { currentUser } from "@clerk/nextjs";
+
+const page = async () => {
+  const user = await currentUser();
+
+  const userInfo = {};
+
+  const userData = {
+    id: "Will come from db...",
+    objectId: userInfo?.id,
+    username: userInfo?.username || user?.username,
+    name: userInfo?.name || user?.firstName + " " + user?.lastName || " ",
+    bio: userInfo?.bio || " ",
+    image: userInfo?.image || user?.imageUrl,
+  };
+
+
   return (
-    <main>
+    <main className="flex mx-auto max-x-3xl flex-col justify-start px-10 py-20">
       <h1 className="head-text">Onboarding</h1>
+      <p className="mt-3 text-base-regular text-light-2">
+        Complete your profile now to use Kommuniti
+      </p>
+
+      <section className="mt-9 bg-dark-2 p-10">
+        <AccountProfile user={userData} btnTitle="Continue" />
+      </section>
     </main>
   );
 };
