d9713bfafa8dca9515d8921541ab5745a8826727
Nishant Kumar Singh <nishant220902@gmail.com>
2023-11-21 13:13:22 +0530

    added mongo db connection

---
+++ lib/mongoose.jsx
@@ -0,0 +1,16 @@
+import mongoose, { mongo } from "mongoose";
+let isConnected = false;
+
+export const connectToDB = async () => {
+  mongoose.set("strictQuery", true);
+  if (!process.env.MONGODB_URL) return console.log("MongoDB URL not found");
+
+  if (isConnected) return console.log("Already connected to MongoDB");
+  try {
+    await mongoose.connect(process.env.MONGODB_URL);
+    isConnected = true;
+    console.log(`Successfully connected to MONGODB URL`);
+  } catch (error) {
+    console.log(error);
+  }
+};
