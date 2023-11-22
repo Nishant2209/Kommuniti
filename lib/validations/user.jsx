6d66a28f12340a7e6a0bb308dd357c82af33a430
Nishant Kumar Singh <nishant220902@gmail.com>
2023-11-22 13:12:41 +0530

    added user validation using yup

--- lib/validations/user.jsx
+++ lib/validations/user.jsx
@@ -1,7 +1,22 @@
-import * as z from "zod";
+import * as yup from "yup";
 
-export const UserValidation = z.object({
-    profile_photo: z.string().url().nonempty(),
-    name: z.string().min(3).max(30),
-    username: z.string().min(3).max(30)
-})
+const UserValidation = yup.object().shape({
+  profile_photo: yup.string().url().required(),
+  name: yup
+    .string()
+    .min(3, "Minimum 3 characters.")
+    .max(30, "Maximum 30 caracters.")
+    .required(),
+  username: yup
+    .string()
+    .min(3, "Minimum 3 characters.")
+    .max(30, "Maximum 30 caracters.")
+    .required(),
+  bio: yup
+    .string()
+    .min(3, "Minimum 3 characters.")
+    .max(1000, "Maximum 1000 caracters.")
+    .required(),
+});
+
+export default UserValidation;
