49722814112c07079ba50144665e7152342583ca
Nishant Kumar Singh <nishant220902@gmail.com>
2023-12-31 02:24:51 +0530

    added Comment component

--- app/(root)/thread/[id]/page.jsx
+++ app/(root)/thread/[id]/page.jsx
@@ -1,4 +1,5 @@
 import ThreadCard from "@/components/card/ThreadCard";
+import Comment from "@/components/forms/Comment";
 import { fetchThreadById } from "@/lib/actions/thread.actions";
 import { fetchUser } from "@/lib/actions/user.actions";
 import { currentUser } from "@clerk/nextjs";
@@ -29,6 +30,31 @@ const page = async ({ params }) => {
           createdAt={thread.createdAt}
           comments={thread.children}
         />
+
+        <div className="mt-7">
+          <Comment
+            threadId={params.id}
+            currentUserImg={user.imageUrl}
+            currentUserId={JSON.stringify(userInfo._id)}
+          />
+        </div>
+
+          <div className="mt-10">
+            {thread?.children?.map((childItem) => (
+              <ThreadCard
+                key={childItem._id}
+                id={childItem._id}
+                currentUserId={user.id}
+                parentId={childItem.parentId}
+                content={childItem.text}
+                author={childItem.author}
+                community={childItem.community}
+                createdAt={childItem.createdAt}
+                comments={childItem.children}
+                isComment
+              />
+            ))}
+          </div>
       </div>
     </section>
   );
