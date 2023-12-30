e003aa5b7b7736844e8494f8a1072bf0d2ba428f
Nishant Kumar Singh <nishant220902@gmail.com>
2023-12-31 02:27:26 +0530

    added action of adding comments to threads and adding a thread

--- lib/actions/thread.actions.jsx
+++ lib/actions/thread.actions.jsx
@@ -1,59 +1,95 @@
 "use server";
 
-import User from "../models/user.model";
-import Thread from "../models/thread.model";
-import { connectToDB } from "../mongoose";
 import { revalidatePath } from "next/cache";
 
-export async function createThreads({ text, author, communityId, path }) {
-  connectToDB();
+import { connectToDB } from "../mongoose";
 
-  const createdThread = await Thread.create({
-    text,
-    author,
-    community: null,
-  });
-
-  //Update user model
-  await User.findByIdAndUpdate(author, {
-    $push: {
-      threads: createdThread._id,
-    },
-  });
-
-  revalidatePath(path);
-}
+import User from "../models/user.model";
+import Thread from "../models/thread.model";
+// import Community from "../models/community.model";
 
 export async function fetchThread(pageNumber = 1, pageSize = 20) {
   connectToDB();
 
+  // Calculate the number of posts to skip based on the page number and page size.
   const skipAmount = (pageNumber - 1) * pageSize;
 
-  const threadQuery = Thread.find({
-    parentId: { $in: [null, undefined] },
-  })
+  // Create a query to fetch the posts that have no parent (top-level threads) (a thread that is not a comment/reply).
+  const threadsQuery = Thread.find({ parentId: { $in: [null, undefined] } })
     .sort({ createdAt: "desc" })
     .skip(skipAmount)
     .limit(pageSize)
-    .populate({ path: "author", model: User })
     .populate({
-      path: "children",
+      path: "author",
+      model: User,
+    })
+    // .populate({
+    //   path: "community",
+    //   model: Community,
+    // })
+    .populate({
+      path: "children", // Populate the children field
       populate: {
-        path: "author",
+        path: "author", // Populate the author field within children
         model: User,
-        select: "_id name parentId image",
+        select: "_id name parentId image", // Select only _id and username fields of the author
       },
     });
 
+  // Count the total number of top-level posts (threads) i.e., threads that are not comments.
   const totalThreadsCount = await Thread.countDocuments({
     parentId: { $in: [null, undefined] },
-  });
+  }); // Get the total count of posts
 
-  const threads = await threadQuery.exec();
+  const threads = await threadsQuery.exec();
 
   const isNext = totalThreadsCount > skipAmount + threads.length;
 
   return { threads, isNext };
+}
+
+export async function createThreads({ text, author, communityId, path }) {
+  try {
+    connectToDB();
+
+    // const communityIdObject = await Community.findOne(
+    //   { id: communityId },
+    //   { _id: 1 }
+    // );
+
+    const createdThread = await Thread.create({
+      text,
+      author,
+    });
+
+    // Update User model
+    await User.findByIdAndUpdate(author, {
+      $push: { threads: createdThread._id },
+    });
+
+    // if (communityIdObject) {
+    //   // Update Community model
+    //   await Community.findByIdAndUpdate(communityIdObject, {
+    //     $push: { threads: createdThread._id },
+    //   });
+    // }
+
+    revalidatePath(path);
+  } catch (error) {
+    throw new Error(`Failed to create thread: ${error.message}`);
+  }
+}
+
+async function fetchAllChildThreads(threadId) {
+  const childThreads = await Thread.find({ parentId: threadId });
+
+  const descendantThreads = [];
+  for (const childThread of childThreads) {
+    const descendants = await fetchAllChildThreads(childThread._id);
+    descendantThreads.push(childThread, ...descendants);
+  }
+
+  return descendantThreads;
 }
 
 export async function fetchThreadById(threadId) {
@@ -70,7 +106,7 @@ export async function fetchThreadById(threadId) {
       //   path: "community",
       //   model: Community,
       //   select: "_id id name image",
-      // }) // Populate the community field with _id and name 
+      // }) // Populate the community field with _id and name
       .populate({
         path: "children", // Populate the children field
         populate: [
@@ -96,5 +132,47 @@ export async function fetchThreadById(threadId) {
   } catch (err) {
     console.error("Error while fetching thread:", err);
     throw new Error("Unable to fetch thread");
+  }
+}
+
+export async function addCommentToThread(threadId, commentText, userId, path) {
+  connectToDB();
+
+  try {
+    // Find the original thread by its ID
+    const originalThread = await Thread.findById(threadId);
+
+    if (!originalThread) {
+      throw new Error("Thread not found");
+    }
+
+    // Create the new comment thread
+    const commentThread = new Thread({
+      text: commentText,
+      author: userId,
+      parentId: threadId, // Set the parentId to the original thread's ID
+    });
+
+    // Save the comment thread to the database
+    const savedCommentThread = await commentThread.save();
+
+    // if (!originalThread.children) {
+    //   originalThread.schema.add({
+    //     children: [{ type: mongoose.Types.ObjectId, ref: "Thread" }],
+    //   });
+    // }
+
+    // Add the comment thread's ID to the original thread's children array
+    await Thread.findByIdAndUpdate(threadId, {
+      $push: { children: savedCommentThread._id },
+    });
+
+    // Save the updated original thread to the database
+    // await originalThread.save();
+
+    revalidatePath(path);
+  } catch (err) {
+    console.error("Error while adding comment:", err);
+    // throw new Error("Unable to add comment");
   }
 }
