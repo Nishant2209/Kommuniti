385a3a447fde0e8f56d4f38f5c58554e698d2f4d
Nishant Kumar Singh <nishant220902@gmail.com>
2023-12-31 02:28:27 +0530

    changed mongoose model for the threads

--- lib/models/thread.model.jsx
+++ lib/models/thread.model.jsx
@@ -1,16 +1,17 @@
 import mongoose from "mongoose";
 
 const threadSchema = new mongoose.Schema({
-  text: { type: String, required: true },
+  text: {
+    type: String,
+    required: true,
+  },
   author: {
     type: mongoose.Schema.Types.ObjectId,
     ref: "User",
     required: true,
   },
-  community: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "Community",
-  },
+  community: { type: mongoose.Schema.Types.ObjectId, ref: "Community" },
+  children: [{ type: mongoose.Schema.Types.ObjectId, ref: "Thread" }],
   createdAt: {
     type: Date,
     default: Date.now,
@@ -18,11 +19,8 @@ const threadSchema = new mongoose.Schema({
   parentId: {
     type: String,
   },
-  children: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "Thread",
-  },
 });
 
 const Thread = mongoose.models.Thread || mongoose.model("Thread", threadSchema);
+
 export default Thread;
