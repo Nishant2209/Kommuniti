5a4ec44cb0856472958ae8663898d30211519029
Nishant Kumar Singh <nishant220902@gmail.com>
2023-12-31 02:22:45 +0530

    added optional chaining

--- app/(root)/page.jsx
+++ app/(root)/page.jsx
@@ -18,11 +18,11 @@ export default async function Home() {
       <h1 className="head-text text-left">Home</h1>
 
       <section className="mt-9 flex flex-col gap-10">
-        {result.threads.length === 0 ? (
+        {result?.threads?.length === 0 ? (
           <p className="no-result">No threads found</p>
         ) : (
           <>
-            {result.threads.map((post) => (
+            {result?.threads?.map((post) => (
               <ThreadCard
                 key={post._id}
                 id={post._id}
